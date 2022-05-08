41065b5fa873b4c01be70b13a5b67ec70a021f24
RosaKarin <50404764+RosaKarin@users.noreply.github.com>
2022-05-08 22:18:56 +0200

    update

--- JS/chart.js
+++ JS/chart.js
@@ -1,3 +1,88 @@
+// Speed visialisation - ITEM1
+google.charts.load('current', {'packages':['gauge']});
+google.charts.setOnLoadCallback(speed);
+
+function speed() {
+    var data = google.visualization.arrayToDataTable([
+        ['Label', 'Value'],
+        ['Speed', 29876]
+    ]);
+
+    var options = {
+          width: 400, height: 180,
+          yellowFrom:35000, yellowTo: 44999,
+          redFrom: 45000, redTo: 50000,
+          minorTicks: 8, max: 50000
+    };
+
+    var chart = new google.visualization.Gauge(document.getElementById('speed'));
+    chart.draw(data, options);
+   
+    setInterval(function() {
+        data.setValue(0, 1, 29876 + Math.round(2000 * Math.random()));
+        chart.draw(data, options);
+    }, 1000);
+}
+    // var gaugeOptions = {min: 0, max: 280, yellowFrom: 200, yellowTo: 250,
+    //   redFrom: 250, redTo: 280, minorTicks: 5};
+    // var gauge;
+
+
+// KM travelled visualisation- ITEM2
+google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Day', 'Travelled km'],
+          ['Mon',  500],
+          ['Tues',  990],
+          ['Wed',  900],
+          ['Thu',  1030],
+          ['Fri',  900],
+          ['Sat',  1200],
+          ['Sun',  1050]
+        ]);
+
+        var options = {
+          title: '',
+          hAxis: {title: 'Day of the week',  titleTextStyle: {color: '#333'}},
+          vAxis: {title: 'Travelled kilometers', minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+// // // Water left visualisation- ITEM3
+//     google.charts.load("current", {packages:['corechart']});
+//     google.charts.setOnLoadCallback(drawChart);
+//     function drawChart() {
+//       var data = google.visualization.arrayToDataTable([
+//         ["Element", "Contents in %", { role: "style" } ],
+//         ["Water", 95, "blue"],
+//       ]);
+
+//       var view = new google.visualization.DataView(data);
+//       view.setColumns([0, 1,
+//                        { calc: "stringify",
+//                          sourceColumn: 1,
+//                          type: "string",
+//                          role: "annotation" },
+//                        2]);
+
+//       var options = {
+//         title: "",
+//         width: 0,
+//         height: 325,
+//         bar: {groupWidth: "95%"},
+//         legend: { position: "center" },
+//       };
+//       var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+//       chart.draw(view, options);
+//   };
+
+// Pie chart about pie of the day - ITEM4
 const pieChart = {
   chart: null,
   data: [
@@ -9,12 +94,10 @@ const pieChart = {
   ],
   element: '#pieChart',
   options: {
-    // title: 'Poll results for pie of the day',
     width: 500,
-    height: 300
+    height: 300,
   }
 };
-
 
 const init = () => {
   pieChart.chart = new google.visualization.PieChart(
@@ -26,11 +109,11 @@ const init = () => {
   );
 };
 
-// https://developers.google.com/chart/interactive/docs/quick_start
 google.charts.load('current', {
   packages: ['corechart'],
   callback: init
 });
+
 
 
 
